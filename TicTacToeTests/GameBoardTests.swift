48d77b1bba9061c05334ca63686beb03ede1930c
Wilma <kima_92@fastmail.com>
2019-11-06 13:02:37 -0500

    Added a test for assuring ourselves that the gameBoard is empty when it is created

--- TicTacToeTests/GameBoardTests.swift
+++ TicTacToeTests/GameBoardTests.swift
@@ -7,14 +7,33 @@
 //
 
 import XCTest
+@testable import TicTacToe
 
 class GameBoardTests: XCTestCase {
-
- 
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    
+    
+    func testCreatingAnEmptyBoard() {
+        
+        let board = GameBoard()
+        
+        XCTAssertNil(board[(x:0, y:0)])
+        XCTAssertNil(board[(x:1, y:0)])
+        XCTAssertNil(board[(x:2, y:0)])
+        XCTAssertNil(board[(x:0, y:1)])
+        XCTAssertNil(board[(x:1, y:1)])
+        XCTAssertNil(board[(x:2, y:1)])
+        XCTAssertNil(board[(x:1, y:2)])
+        XCTAssertNil(board[(x:2, y:2)])
+        
+        for x in 0..<3 {
+            for y in 0..<3 {
+                XCTAssertNil(board[(x,y)], "The entry at (\(x) , \(y)) was\(board[(x, y)]!)() instead of nil")
+            }
+        }
+        
+        
+       // board[( x: 0, y: 0 )]
     }
-
-
+    
+    
 }
