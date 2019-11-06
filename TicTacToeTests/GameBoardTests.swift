59fb123d32b4277b16fe99828a5710413c012d27
Wilma <kima_92@fastmail.com>
2019-11-06 12:34:30 -0500

    Added GameBoardTest to TicTicToeTest folder

---
+++ TicTacToeTests/GameBoardTests.swift
@@ -0,0 +1,20 @@
+//
+//  GameBoardTests.swift
+//  TicTacToeTests
+//
+//  Created by macbook on 11/6/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import XCTest
+
+class GameBoardTests: XCTestCase {
+
+ 
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+
+}
